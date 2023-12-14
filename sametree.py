ce2765fd7dd982487cfbb5bb5dc6e920a6fd364a
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-12-13 21:55:37 -0500

    notes for trees

--- sametree.py
+++ sametree.py
@@ -5,6 +5,8 @@ class TreeNode:
         self.left = left
         self.right = right
 
+#TODO: add balanced tree class
+
 class DataTreeNode(TreeNode):
 
     def __init__(self, val=0, left=None, right=None, adData=None):
@@ -61,6 +63,9 @@ class Solution:
     #def invertTree(self, root: TreeNode) -> TreeNode:
         # [2,1,3] -> [2,3,1]
         # [4,2,7,1,3,6,9] -> [4,7,2,9,6,3,1]
+
+    #TODO: add DFS and BFS print
+    
 
 def main():
     #Creating different Trees
